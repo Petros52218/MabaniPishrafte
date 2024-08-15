aeef6d6c0e2e46bf0021b6154aa3d2f91a7021c8
Petros Arakelyan <petros.arakelyan52218@outlook.de>
2024-08-15 06:08:28 +0200

    class & objects

--- MabaniPishrafteJava/src/test/java/MashinTest.java
+++ MabaniPishrafteJava/src/test/java/MashinTest.java
@@ -1,45 +1,98 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MashinTest {
+
+    @Test
+    void Mashin_Bayad_Harekat_Konad_Vaghti_Dar_Baste_Ast_va_Mashin_Roshan_Ast() {
+
+        //Given
+        Mashin benz = new Mashin( "Benz");
+        Mashin volvo = new Mashin("volvo");
+
+
+        //When
+        boolean ayaHarekatMikonad = benz.ayaMashinDarHaleHarekatAst();
+        volvo.roshan();
+        boolean ayaVolvoHarekatMikonad = volvo.ayaMashinDarHaleHarekatAst();
+
+
+
+        //Then
+        assertFalse(ayaHarekatMikonad);
+        assertTrue(ayaVolvoHarekatMikonad);
+
+
+
+
+        
+        // Object Mashin
+//        Mashin mashin = new Mashin();
+//        mashin.BazKardanDAR();
+//        mashin.BastanDar();
+//        System.out.println("Dar Mashin Baz Ast? " + mashin.ayaDarMashinBazAst);
+//        mashin.ayaMashinDarHaleHarekatAst();
+//        System.out.println("--------------------------------------");
+//        Mashin pride = new Mashin("pride");
+//        pride.roshan();
+//        pride.BazKardanDAR();
+//        pride.ayaMashinDarHaleHarekatAst();
+    }
+
     //Nested Class
     class Mashin{
+
+        // Constructor
+        Mashin(){
+            out.println("Yek Mashin bedune Dade Sakhte Shod");
+        }
+        Mashin(String mark){
+            this.mark = mark;
+            out.println("Yek Mashin " + this.mark + " Sakhte Shod");
+
+        }
+
 
         //Dade: Shakhsiathaa
         boolean ayaMashinRoshanAst;
         boolean ayaDarMashinBazAst;
-        double toul;
-        double arz;
         String mark;
 
 
 
         //Tabe(Methode): Raftaarhaa
         void roshan() {
-            System.out.println("Mashin roshan ast");
+            out.println("Mashin roshan ast");
             ayaMashinRoshanAst = true;
         }
 
         void khamoush(){
-            System.out.println("Mashin khamoush ast");
+            out.println("Mashin khamoush ast");
             ayaMashinRoshanAst = false;
         }
 
         void BazKardanDAR(){
-            System.out.println("Dar baz ast");
+            out.println("Dar baz ast");
             ayaDarMashinBazAst = true;
         }
 
         void BastanDar(){
-            System.out.println("Dar baste shod");
+            out.println("Dar baste shod");
             ayaDarMashinBazAst = false;
         }
 
-        void Harekat(){
+        boolean ayaMashinDarHaleHarekatAst(){
             // dar baste bashad va mashin roshan bashad (rule)
             if (!ayaDarMashinBazAst && ayaMashinRoshanAst){
-                System.out.println("MashinDarHaleHarekatAst ");
+                out.println("MashinDarHaleHarekatAst ");
+                return true;
             } else {
-                System.out.println("Mashin Park ast");
+                out.println("Mashin Park ast");
+                return false;
             }
         }
 
